40ee1a00b4f89b10edfbf62d841cf98a2e3bf9b6
YoshiAsk <jjask7@gmail.com>
2020-06-13 22:20:35 -0700

    Add project files.

---
+++ Microsoft.Toolkit.Uwp.UI.Controls.Ribbon/RibbonColumn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls.Ribbon
+{
+    public sealed class RibbonColumn : ListViewBase
+    {
+        public RibbonColumn()
+        {
+            this.DefaultStyleKey = typeof(RibbonColumn);
+        }
+
+        public static readonly DependencyProperty SpacingProperty = DependencyProperty.Register(
+            "Spacing",
+            typeof(double),
+            typeof(RibbonColumn),
+            new PropertyMetadata(1.0)
+        );
+        public double Spacing {
+            get { return (double)GetValue(SpacingProperty); }
+            set { SetValue(SpacingProperty, value); }
+        }
+    }
+}
---
+++ Microsoft.Toolkit.Uwp.UI.Controls.Ribbon/RibbonGroup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Microsoft.Toolkit.Uwp.UI.Controls.Ribbon
+{
+    public sealed class RibbonGroup : ListViewBase
+    {
+        public RibbonGroup()
+        {
+            this.DefaultStyleKey = typeof(RibbonGroup);
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
+            "Label",
+            typeof(string),
+            typeof(RibbonGroup),
+            new PropertyMetadata("")
+        );
+        public string Label {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+    }
+}
