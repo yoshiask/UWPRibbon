fe46c77ed6f56a1e7ddcb80ec6184cfd0b030dbb
Lance <lance.mccarthy@progress.com>
2020-06-14 13:16:41 -0400

    Quick fix to Generic and Ribbon controls

--- Microsoft.Toolkit.Uwp.UI.Controls.Ribbon/RibbonButton.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls.Ribbon/RibbonButton.cs
@@ -5,19 +5,33 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Ribbon
 {
     public sealed class RibbonButton : Button
     {
+        private StackPanel contentPanel;
+        private TextBlock labelTextBlock;
+
         public RibbonButton()
         {
             this.DefaultStyleKey = typeof(RibbonButton);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (this.GetTemplateChild("PART_ContentPanel") is StackPanel sp)
+                this.contentPanel = sp;
+
+            if (this.GetTemplateChild("PART_LabelBlock") is TextBlock tb)
+                this.labelTextBlock = tb;
         }
 
         public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
             "Icon", typeof(IconElement), typeof(RibbonButton), new PropertyMetadata(new SymbolIcon(Symbol.Add)));
 
         public static readonly DependencyProperty LabelProperty = DependencyProperty.Register(
-            "Label", typeof(string), typeof(RibbonButton), new PropertyMetadata(string.Empty));
+            "Label", typeof(string), typeof(RibbonButton), new PropertyMetadata(string.Empty, OnLabelValueChanged));
 
         public static readonly DependencyProperty IsCompactProperty = DependencyProperty.Register(
-            "IsCompact", typeof(bool), typeof(RibbonButton), new PropertyMetadata(default(bool)));
+            "IsCompact", typeof(bool), typeof(RibbonButton), new PropertyMetadata(default(bool), OnIsCompactChanged));
 
         public IconElement Icon 
         {
@@ -35,6 +49,30 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls.Ribbon
         {
             get => (bool)GetValue(IsCompactProperty);
             set => SetValue(IsCompactProperty, value);
+        }
+
+        private static void OnLabelValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is RibbonButton self && e.NewValue is string labelText && self.labelTextBlock != null)
+            {
+                self.labelTextBlock.Visibility = string.IsNullOrEmpty(labelText)
+                    ? Visibility.Collapsed
+                    : Visibility.Visible;
+            }
+        }
+
+        private static void OnIsCompactChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is RibbonButton self && e.NewValue is bool val && self.contentPanel != null)
+            {
+                self.contentPanel.HorizontalAlignment = val
+                    ? HorizontalAlignment.Left
+                    : HorizontalAlignment.Center;
+
+                self.contentPanel.Orientation = val
+                    ? Orientation.Horizontal
+                    : Orientation.Vertical;
+            }
         }
     }
 }
